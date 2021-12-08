80df5fc5d03eb0089071eacb23c19924c7744be3
Eva <evavanderscheer@live.nl>
2021-12-08 14:50:24 +0100

    mind castle

---
+++ AlohamortaGame/Assets/Scripts/Mind Castle/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Vector2 movement;
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
---
+++ AlohamortaGame/Assets/Scripts/Mind Castle/ScrollManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ScrollManager : MonoBehaviour
+{
+    private float width;
+    private SpriteRenderer spriteRenderer;
+    public Transform target;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        width = spriteRenderer.sprite.bounds.size.x * transform.localScale.x;
+    }
+
+    private void Update()
+    {
+        //if the player can't see this background anymore, so if the distance is bigger than the width
+        if (target.position.x - transform.position.x > width)
+        {
+            LoopForward();
+        }
+
+        if (target.position.x - transform.position.x < -width)
+        {
+            LoopBackwards();
+        }
+    }
+
+    private void LoopForward()
+    {
+        Vector2 toFront = new Vector2(width * 2f, 0);
+
+        transform.position = (Vector2)transform.position + toFront;
+    }
+
+    private void LoopBackwards()
+    {
+        Vector2 toBack = new Vector2(width * -2f, 0);
+
+        transform.position = (Vector2)transform.position + toBack;
+    }
+}
