4c8a42835febdd5ed62d741ec90e4fb74db113cc
Eva <evavanderscheer@live.nl>
2022-01-25 13:41:46 +0100

    intro phone

---
+++ AlohamortaGame/Assets/Scripts/SubsBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubsBehaviour : MonoBehaviour
+{
+    public Text Subtitle;
+    public List<Subtitle> Subtitles;
+    public AudioClip Clip;
+    public AudioSource Player;
+    public Button ContinueButton;
+
+    private void Start()
+    {
+        Player.clip = Clip;
+        Play();
+    }
+    public void Play()
+    {
+        StartCoroutine(SubRoutine());
+    }
+
+    public IEnumerator SubRoutine()
+    {
+        Player.Play();     
+        foreach(var sub in Subtitles)
+        {
+            Subtitle.text = sub.Sub;
+            yield return new WaitForSeconds(sub.Duration);
+        }
+        End();
+    }    
+
+    private void End()
+    {
+        Subtitle.text = "";
+        ContinueButton.gameObject.SetActive(true);
+    }
+}
---
+++ AlohamortaGame/Assets/Scripts/Subtitle.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+[Serializable]
+public class Subtitle
+{
+    public string Sub;
+    public float Duration;   
+}
+
