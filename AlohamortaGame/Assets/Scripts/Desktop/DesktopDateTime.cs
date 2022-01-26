b85084379923e77119f48d4eedeecf1016d5c916
Eva <evavanderscheer@live.nl>
2022-01-26 19:28:58 +0100

    date change

--- AlohamortaGame/Assets/Scripts/Desktop/DesktopDateTime.cs
+++ AlohamortaGame/Assets/Scripts/Desktop/DesktopDateTime.cs
@@ -2,7 +2,7 @@
 {
     public static int Day { get; set; }
     public static int month = 10;
-    public static int year = 2021;
+    public static int year = 2022;
     public static int Hour { get; set; }
     public static int Minute { get; set; }
 }
